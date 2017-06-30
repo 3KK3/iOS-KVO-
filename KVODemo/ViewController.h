9bb4f1d2ea73aec80aae7daa44acf3b386d17c68
YZY_PRO <976809588@qq.com>
2017-06-30 18:15:55 +0800

    Initial Commit

---
+++ KVODemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KVODemo
+//
+//  Created by YZY on 2017/6/30.
+//  Copyright © 2017年 YZY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ KVODemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  KVODemo
+//
+//  Created by YZY on 2017/6/30.
+//  Copyright © 2017年 YZY. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
